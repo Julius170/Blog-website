0edef1b1e960f95a10a92171666d579f9ba477f7
Julius170 <juliusyoola5@gmail.com>
2022-05-09 22:41:32 +0100

    App Comleted

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,6 +17,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const posts = [];
+
+
+
 app.get('/', function(req, res) {
   res.render("home", {text: homeStartingContent})
 });
@@ -24,18 +30,38 @@ app.get('/about', function(req, res) {
 });
 
 app.get('/contact', function(req, res) {
-  res.render('contact' , { contactMeContent: contactContent})
-})
+  res.render('contact' , { contactMeContent: contactContent })
+});
 
 app.get('/compose', function(req, res) {
   res.render("compose")
-})
+});
 
-app.post('/compose', function(req, res) {
-  console.log(req.body.newpost)
-})
+app.post('/', function(req, res) {
+  
+  const blogDetails = {
+    blogTitle: req.body.title,
+    blogContent: req.body.newpost}
+  posts.push(blogDetails);
+  res.redirect('/');
+});
 
+app.get('/posts/:postsName', function(req, res) {
+  const requestedTitle = _lowerCase(req.params.postsName);
+  
+
+  posts.forEach(function(post) {
+    const storedTitle = _lowerCase(post.blogTitle);
+
+    if (requestedTitle === storedTitle) {
+      res.render('/post', {content:posts.blogContent, title:posts.blogTitle});
+    };
+  });
+
+
+  res.redirect('/');
+});
 
 app.listen(3001, function() {
-  console.log("Server started on port  http://localhost:3001/");
+  console.log("Server started on port http://localhost:3001/");
 });
